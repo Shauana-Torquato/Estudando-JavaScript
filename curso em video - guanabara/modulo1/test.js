e4a3a0fdd6aed16e1d13c9182b3ea9b0498035c3
Shauana-Torquato <shauana.torquato@outlook.com>
2023-10-05 22:17:27 -0300

    Update test.js
    add commands from class 01

---
+++ curso em video - guanabara/modulo1/test.js
@@ -0,0 +1,8 @@
+//estou apenas escrevendo as linhas de comando que estou aprendendo
+
+
+//criando uma janelinha pop up assim que se inicia a pagina
+<script>
+    
+    window.alert('vou parar por aqui que já estou passada')
+</script>
