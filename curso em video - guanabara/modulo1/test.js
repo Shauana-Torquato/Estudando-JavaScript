39cd44ede2fa9bea1383bd5214fe88c705664df9
Shauana-Torquato <shauana.torquato@outlook.com>
2023-10-06 21:36:23 -0300

    Update test.js
    adicionando comandos

--- curso em video - guanabara/modulo1/test.js
+++ curso em video - guanabara/modulo1/test.js
@@ -1,8 +1,26 @@
 //estou apenas escrevendo as linhas de comando que estou aprendendo
 
+//lembrando qeu pra inserir o javascript no html tem que abris <script></script>
 
 //criando uma janelinha pop up assim que se inicia a pagina
-<script>
-    
-    window.alert('vou parar por aqui que já estou passada')
-</script>
+
+
+
+    window.alert('vamos fazer este último exercício da noite que já estou com dor de tanto digitar aqui e preciso parar')
+
+    window.confirm('hoje a gente tá só o pó, né?')
+
+    window.prompt('o que a gente faz com esse cansaço?')
+
+    window.alert("sua sugestão é " + resposta + " ? kkkkkkk")
+
+
+// e com variáveis?
+
+    var resposta = window.prompt('o que a gente faz com esse cansaço?')
+
+
+
+
+
+
